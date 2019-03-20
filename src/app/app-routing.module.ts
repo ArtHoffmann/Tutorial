0fc192a9323284376d875b68996f95db393ea241
DESKTOP-896C90P\Artur <Hoffmann.artur93@gmail.com>
2019-03-20 09:29:18 +0100

    chore(): custom form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,7 +34,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     useHash: true,
-    preloadingStrategy: PreloadAllModules,
+    // preloadingStrategy: PreloadAllModules,
   })],
   exports: [RouterModule]
 })
--- src/app/visualisation/configuration/components/user-add/user-add.component.ts
+++ src/app/visualisation/configuration/components/user-add/user-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-add',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAddComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      userIdentification: ['', Validators.required],
+      selectedLine: null
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+    // do smth
+    }
+  }
+
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched)
+    );
   }
 
 }
